5903f2b68b7751bc55f740314d999aa424ff3c3d
Shubhashish Basnet <subhashish.basnet@neosoftware.com.np>
2023-09-13 17:47:21 +0545

    update

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PollsController } from './polls.controller';
+import { PollsService } from './polls.service';
 
 @Module({
   imports: [ConfigModule],
-  controllers: [],
-  providers: [],
+  controllers: [PollsController],
+  providers: [PollsService],
 })
 export class PollsModule {}
---
+++ server/src/polls/polls.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePollFields, JoinPollFields, RejoinPollFields } from './types';
+import { createPollID, createUserID } from 'src/ids';
+
+@Injectable()
+export class PollsService {
+  async createPoll(fields: CreatePollFields) {
+    const pollID = createPollID();
+    const userID = createUserID();
+
+    return {
+      ...fields,
+      userID,
+      pollID,
+    };
+  }
+
+  async joinPoll(fields: JoinPollFields) {
+    const userID = createUserID();
+
+    return {
+      ...fields,
+      userID,
+    };
+  }
+
+  async rejoinPoll(fields: RejoinPollFields) {
+    return fields;
+  }
+}
